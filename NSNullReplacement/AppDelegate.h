b9b6ff96d8171a131b67052746195407bbf3c06f
Konstantyn Byhkalo <kb@anahoret.com>
2016-01-14 13:28:14 +0200

    Initial Commit

---
+++ NSNullReplacement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSNullReplacement
+//
+//  Created by Byhkalo Konstantyn on 14.01.16.
+//  Copyright © 2016 Anadea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSNullReplacementTests/NSNullReplacementTests.m
@@ -0,0 +1,39 @@
+//
+//  NSNullReplacementTests.m
+//  NSNullReplacementTests
+//
+//  Created by Byhkalo Konstantyn on 14.01.16.
+//  Copyright © 2016 Anadea. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSNullReplacementTests : XCTestCase
+
+@end
+
+@implementation NSNullReplacementTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
